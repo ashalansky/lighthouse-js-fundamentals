d1fffdfb16db2f55bf7963a0e019b72ddf344e88
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-07 04:16:50 +0000

    updated

--- conditionals-whichSchool.js
+++ conditionals-whichSchool.js
@@ -10,3 +10,10 @@ const whichSchool = function(age) {
   }
 }
 console.log(whichSchool(19));
+
+for (var i=1; i < 101; i++){
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
+}
