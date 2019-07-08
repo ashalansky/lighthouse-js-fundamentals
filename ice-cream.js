a417f617c063a6477fb7f0dad0f3789de6239ee2
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-08 05:09:56 +0000

    listing an array of ice cream flavours

--- ice-cream.js
+++ ice-cream.js
@@ -3,8 +3,6 @@ console.log(iceCreamFlavours);
 
 iceCreamFlavours.push("root beer");
 
-console.log(iceCreamFlavours);
-
 console.log(iceCreamFlavours[0]);
 
 console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
